a32db3562e5d4b93e14a1bbdd4e2e2f3995dd4b4
Mabvsuri <97302158+Mabvsuri@users.noreply.github.com>
2024-10-22 13:46:11 -0500

    Update CarritoController.java

--- src/main/java/pe/trujillo/ropa/TiendaRopaOnline/Controller/CarritoController.java
+++ src/main/java/pe/trujillo/ropa/TiendaRopaOnline/Controller/CarritoController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import jakarta.servlet.http.HttpSession;
 import pe.trujillo.ropa.TiendaRopaOnline.Model.Producto;
